afc2d2fe19ceefe4b867912d51fda970dee7a75e
SitNis <68220034+SitNis@users.noreply.github.com>
2020-11-04 15:38:28 +0300

    Add files via upload

--- js/Element.js
+++ js/Element.js
@@ -9,7 +9,7 @@ class Element{
         const div = document.createElement('div')
         const title = document.createElement('h3')
         const textarea = document.createElement('textarea')
-        const priority = document.createElement('span')
+        const priority = document.createElement('p')
         title.insertAdjacentText("afterbegin",this.name)
         textarea.value = this.text
         textarea.setAttribute('id','newTextarea')
@@ -20,17 +20,26 @@ class Element{
         div.insertAdjacentElement("beforeend",textarea)
         div.insertAdjacentElement("beforeend",priority)
         div.setAttribute('id',counter)
-        // div.insertAdjacentHTML("beforeend","<button id='readyButton'> Сделано </button>")
         const button2 = document.createElement('button')
         button2.setAttribute('id','readyButton')
         button2.insertAdjacentText("beforeend",'Готово')
         
         button2.addEventListener('click', (event) =>{
             div.childNodes[div.childNodes.length-1].remove()
+            div.insertAdjacentElement("beforeend",button3)
             document.getElementById('compl').insertAdjacentElement("beforeend",div)
-
-            // div.remove()
+            
+        });
+        const button3 = document.createElement('button')
+        button3.setAttribute('id','errorButton')
+        button3.insertAdjacentText("beforeend",'Не готово')
+        button3.addEventListener('click',(event) =>{
+            div.childNodes[div.childNodes.length-1].remove()
+            div.insertAdjacentElement("beforeend",button2)
+            document.getElementById('containerId').insertAdjacentElement("beforeend",div)
+            
         })
+
         div.insertAdjacentElement("beforeend",button2)
         this.element = div
         counter++
